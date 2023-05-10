6b6ae3e65bd75013de7bc69a67e670c6dcf8ed0e
Castro Argüello <m.castro.arguello@accenture.com>
2023-05-10 19:28:44 -0300

    agregando servidor 3000

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,10 +1,40 @@
 import express from "express";
+import fs from 'fs';
 
-const app = express (); 
+const app = express();
+const port = 3000;
 
-app.get('/products', (req, res) =>{
-    res.send(`<h1 style= "color:red;"> Bienvenido a express </h1>`)
+const productosPath = './files/Productos.json';
+
+app.use(express.json());
+
+app.get('/productos', (req, res) => {
+  const productos = obtenerProductos();
+  res.json(productos);
 });
 
-app.listen (8080, () => console.log ('listening 8080'));
+app.get('/productos/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const productos = obtenerProductos();
+  const producto = productos.find((p) => p.id === id);
 
+  if (producto) {
+    res.json(producto);
+  } else {
+    res.status(404).json({ error: 'Producto no encontrado' });
+  }
+});
+
+function obtenerProductos() {
+  if (fs.existsSync(productosPath)) {
+    const data = fs.readFileSync(productosPath, 'utf-8');
+    const productos = JSON.parse(data);
+    return productos;
+  } else {
+    return [ "Estos son los productos"];
+  }
+}
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
