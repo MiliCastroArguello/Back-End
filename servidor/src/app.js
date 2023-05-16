449878bb2a16dd72258ae34bd4a46d83722bd266
Castro Argüello <m.castro.arguello@accenture.com>
2023-05-16 12:52:47 -0300

    corrigiendo errores

--- servidor/src/app.js
+++ servidor/src/app.js
@@ -1,40 +1,68 @@
-import express from "express";
+import express from 'express';
 import fs from 'fs';
+import ProductManager from './ProductManager.js';
+
+
+import ProductManager from './Desafio3/ProductManager.js';
+
+const manager = new ProductManager();
+
+const Carrito = async () => {
+    let product1 = {
+        title: 'Samsung',
+        description: 'telefono',
+        price: 580,
+        thumbnail: 'sin imagen',
+        code: 105,
+        stock: 3,
+    };
+
+    let product2 = {
+        title: 'iphone',
+        description: 'telefono',
+        price: 500,
+        thumbnail: 'sin imagen',
+        code: 125,
+        stock: 2,
+    };
+
+    let product3 = {
+        title: 'Kindle',
+        description: 'Leer',
+        price: 550,
+        thumbnail: 'sin imagen',
+        code: 130,
+        stock: 1,
+    };
+
+    await manager.crearProductos(product1);
+    await manager.crearProductos(product2);
+    await manager.crearProductos(product3);
+
+    let result = await manager.mostrarProductos();
+
+    console.log(result);
+};
+
+Carrito();
 
 const app = express();
-const port = 3000;
 
-const productosPath = './files/Productos.json';
+const productManager = new ProductManager();
 
-app.use(express.json());
+app.get('/products', async (req, res) => {
+  try {
+    const products = await productManager.mostrarProductos();
 
-app.get('/productos', (req, res) => {
-  const productos = obtenerProductos();
-  res.json(productos);
-});
-
-app.get('/productos/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const productos = obtenerProductos();
-  const producto = productos.find((p) => p.id === id);
-
-  if (producto) {
-    res.json(producto);
-  } else {
-    res.status(404).json({ error: 'Producto no encontrado' });
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occur' });
   }
 });
 
-function obtenerProductos() {
-  if (fs.existsSync(productosPath)) {
-    const data = fs.readFileSync(productosPath, 'utf-8');
-    const productos = JSON.parse(data);
-    return productos;
-  } else {
-    return [ "Estos son los productos"];
-  }
-}
-
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+    const port = 8080; 
+    app.listen(port, () => {
+         console.log(`listening to port ${port}`);
 });
+
